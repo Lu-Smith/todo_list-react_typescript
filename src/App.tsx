823c0a63744f87b450a272c72a68af538b6f92d2
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-05 18:05:16 +0100

    Created App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {FC} from 'react';
 import './App.css';
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      Hello World
+    <div className='App'>
+      <div className='header'>
+        <div>
+           <input type='text' placeholder='Task...'/>
+           <input type='number' placeholder='Deadline (in Days)...'/>
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className='todoList'></div>
     </div>
   );
 }
